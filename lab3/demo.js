0cc89a38e35b2e68a152c3e9c8d23fa6605b55dc
Tomasz Szandala <Tomasz.Szandala@gmail.com>
2021-03-24 08:23:17 +0100

    Lab3 dla Was

---
+++ lab3/demo.js
@@ -0,0 +1,47 @@
+"use strict"
+
+const evaluate = () => {
+    let value = document.getElementById("number-a").value;
+    if(value === "") {
+        console.log("A jest puste");
+        return
+    }
+    const a = Number(value);
+
+    value = document.querySelector("#number-b").value;
+    if(value === "") {
+        console.log("B jest puste");
+        return
+    }
+    const b = Number(value);
+    if (isNaN(a) || isNaN(b)){
+        // console.log("Co najmniej jedna liczba jest NaN");
+        const modal = document.getElementById("modal");
+        modal.style.display = "block";
+        modal.addEventListener("click", (event) =>{
+            event.target.style.display = "none";
+        });
+        return
+    }
+
+    return [a,b];
+}
+
+const adder = () => {
+    const tempVar =  evaluate();
+    let a,b;
+    if (tempVar) {
+        [a,b] = tempVar;
+    }else
+        return
+
+    //debugger
+    console.log(a+b);
+    const newP = document.createElement("p");
+    newP.innerHTML = `${a} + ${b} = ${a+b}`;
+
+    $("#operations-list").append(newP);
+    // $("#sub").hide("fast");
+
+
+}
