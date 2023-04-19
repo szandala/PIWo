c959d7bb086c6d83415a0447936431297b5150d1
Tomasz Szandala <Tomasz.Szandala@gmail.com>
2023-04-19 10:07:55 +0200

    Cleanup

--- todo_redux/src/Data/UserContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-const UserContext = createContext(["", ()=>{}]);
-//     {
-//     userName: "",
-//     setUserName: () => {}
-// })
-
-export default UserContext;
--- todo_test/src/pages/Home.jsx
+++
@@ -1,6 +0,0 @@
-const Home = () => {
-
-    return <h2>This is home page</h2>
-}
-
-export default Home;
